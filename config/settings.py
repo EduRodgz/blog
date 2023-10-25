e53ad3e81211c6194697cf986831276be20e00c5
eduardo rodriguez jr <eduardo@Eduardo>
2023-10-24 22:02:44 -0500

    added heroku req

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+6%=my&3lsbjh)(%eq&%2201ob)pp41t-f@(n7s@xsg_5!(1x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
 
