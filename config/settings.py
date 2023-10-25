6fad4f0ff9cef8636aa4315d424ba4b438a23c4c
eduardo rodriguez jr <eduardo@Eduardo>
2023-10-24 21:29:29 -0500

    tuesday

--- config/settings.py
+++ config/settings.py
@@ -122,4 +122,8 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
